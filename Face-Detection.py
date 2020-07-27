0bb22d7a5025443e780a0149678862777cce0830
Mijail <mijail.devops@gmail.com>
2020-07-26 21:08:36 -0500

    mejora en codigo comentarios

--- Face-Detection.py
+++ Face-Detection.py
@@ -19,22 +19,35 @@ import base64
 from base64 import b64encode
 #Request
 import requests
-#Opencv
-import cv2
+
 
 #///////////////////////////////////////////
 # Get Hostname
 #///////////////////////////////////////////
 Hostname = socket.gethostname()
-print('Runing... Nats Subscriber')
+print('[Face Detection] is Running... Nats Server - Host: ', Hostname)
 
-#task y 
 
+#///////////////////////////////////////////
+# #crear carpeta
+#///////////////////////////////////////////
+try:
+    FolderFaceDetection = "C:/File-Nats/Face-Detection"
+    FolderSubscriber = "C:/File-Nats/Face-Detection/Subscriber"
+    FolderPublisher = "C:/File-Nats/Face-Detection/Publisher"
+    makedirs(FolderFaceDetection)
+    makedirs(FolderSubscriber)
+    makedirs(FolderPublisher)
+except FileExistsError:
+    print ("      - Backup Folders Exist")
+
+
+# funcion recibe el mensaje del Sensor Procesor 
 async def FrameProces (Secuencia, Data):
 
-    print ('- Task1 - Get Data Sensor Processor')
+    print(' - Connected to channel [VSBLTY-DATA-TEMP] as subscriber ')
     # Secuencia Mensaje
-    print ("    - Message Received: ", str(Secuencia))
+    print ("      - [subscriber] Message received from 'VSBLTY-DATA-TEMP': -", str(Secuencia))
 
     # Decode UTF-8 bytes mensaje Recibido 
     # to double quotes to make it valid JSON
@@ -65,10 +78,10 @@ async def FrameProces (Secuencia, Data):
     Temp = mensaje["Temp"]
     FrameTimestamp = mensaje["FrameTimestamp"]
 
-    print ("       - ", Hostname)
+    """ print ("       - ", Hostname)
     print ("       - ", FrameTimestamp)
     print ("       - ", Temp)
-    print ("       - ", Data[:10])
+    print ("       - ", Data[:10]) """
 
     #/////////////////////////////////////////////////////////////////////
     # Base 64
@@ -82,41 +95,38 @@ async def FrameProces (Secuencia, Data):
     #print (type(ImagenBites))
 
     #//////////////////////////////////////////////////////////////////////
-    # Send request
+    # Send request API-VSBLTY
     #/////////////////////////////////////////////////////////////////////
     files = {'file': ImagenBites}  
     #POST to API
     url = "http://openvino-api.vsblty.support:8099/process?raw"
     r=requests.post(url,files=files)
 
-    #Print the results
-
+    # the results
     respuesta = str(r.content.decode())
+    print('      - [API] Sending Frame with the Detected Person to VSBLTY')
 
+    # build dictionary with data to publish
     data = {}
-
     data['FrameTimestampId'] = str(FrameTimestamp)
     data['SecuenciaMsg'] = str(Secuencia)
     data['HostnamePublisher'] = str(Hostname)
     data['Data'] = str(Data)
     data['Temp'] = str(Temp)
 
-
+    # Path
     dir = 'C:/File-Nats/Face-Detection/Publisher'  
 
     # Estructura para el Nombre del Archivo
     file_name = str(Secuencia) + " - [Face-Detection] - [Publisher] - " + str(timestampStr)  + ".json"
 
     # Decode UTF-8 bytes mensaje Recibido 
-    # to double quotes to make it valid JSON
-    #JsonNats = respuesta.data.decode('utf8').replace("'", '"')
 
     # mensaje es formatted JSON
     mensaje = json.loads(respuesta)
 
+    # Add result Request Api Vsblty
     data['FaceDetection'] = mensaje
-
-
 
     # Crear Archivo .json con los datos del mensaje Recibido
     with open(os.path.join(dir, file_name), 'w') as file:
@@ -124,16 +134,16 @@ async def FrameProces (Secuencia, Data):
 
     return data
 
-
-
-
-
+#/////////////////////////////////////////////////////////////////
+# Funcion error de Conexion
+#/////////////////////////////////////////////////////////////////
 async def error_cb(e):
     print("Error:", e)
 
+#/////////////////////////////////////////////////////////////////
+# Conexion a Nats Server
+#/////////////////////////////////////////////////////////////////
 async def run(loop):
-
-    #
 
     nc = NATS()
     sc = STAN()
@@ -148,25 +158,21 @@ async def run(loop):
 
     # Start session with NATS Streaming cluster using
     # the established NATS connection.
-    await sc.connect("vsblty-cluster", "client-1235487854", nats=nc)
+    await sc.connect("vsblty-cluster", "client-11b77c0e-6187-4855-acda-3b41e9333e34", nats=nc)
     
-
     async def cb(msg):
 
+        # Creando task, llamando a la Funcion FrameProces
         task1 = await loop.create_task(FrameProces(msg.seq, msg.data))
-        print('task')
-        print (type(task1))
 
-
-
+        #/////////////////////////////////////////////////////////////////////////////
         # Publicar Mensaje con el resultado en el canal VSBLTY-DATA-FACE
+        #/////////////////////////////////////////////////////////////////////////////
         ChannelNats = 'VSBLTY-DATA-FACE'
         # Definir Channel y Mensaje
         await sc.publish(ChannelNats, bytes(str(task1), 'utf8'))
-        # Sleep 
-
-
-        
+        print ("      - [publisher] -  Sending Message To Channel 'VSBLTY-DATA-FACE': - " + str(msg.seq))
+        print ('')
 
     # Subscribe to get all messages from the beginning.
     await sc.subscribe("VSBLTY-DATA-TEMP", start_at='first', cb=cb)
